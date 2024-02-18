7fd7cc93dc185d01148602e466be2e10e0085c01
capeofhope <98865989+capeofhope@users.noreply.github.com>
2024-02-18 07:02:09 +0000

    lecture5_done

---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+
+// alert("Hello");// we are using nodejs,not browser;
+console.log(3+3);//code readibility should be high;
+console.log("Mrinal");
+let age=5;
+//number==2 to power 53
+//bigint
+//string =>""
+//boolean=>true/false;
+//null=>standalone value,object
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof(null));
+console.log(typeof undefined);
