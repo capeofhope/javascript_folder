fdf9db913afc161aae644d2b1ec493b676ef0b51
capeofhope <98865989+capeofhope@users.noreply.github.com>
2024-02-18 16:33:55 +0000

    stack_and_heap

--- 01_BASICS/dataTypes_summary.js
+++ 01_BASICS/dataTypes_summary.js
@@ -12,7 +12,7 @@ let userEmail;
 
 const id=Symbol("1232");
 const id2=Symbol("1232");
-console.log(id==id2);//false;
+// console.log(id==id2);//false;
 
 
 
@@ -27,4 +27,26 @@ let myObj={
 const myfunction=()=>{
     console.log("HEllo World")
 }
-console.log(typeof myfunction);
+// console.log(typeof myfunction);
+
+
+//************************************************//
+
+//Stack(Primitive),Heap(Non-Primitive)
+
+let myName="Mrinal"//go in stack
+let anotherName=myName;
+anotherName="chaincode";
+
+console.log(anotherName);
+console.log(myName);
+
+let userOne={
+    email:"mrinal@gmail.com",
+    upi:"user@sbi"
+}
+
+let userTwo=userOne;
+userTwo.email="mri@gmail.com";//changes the intial object(passed by reference)
+
+console.log(userOne);
