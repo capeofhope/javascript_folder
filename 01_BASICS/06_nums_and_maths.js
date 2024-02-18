eb3812f296f7b413bed96ffd463d3b3e92449bbc
capeofhope <98865989+capeofhope@users.noreply.github.com>
2024-02-18 17:25:11 +0000

    maths_used

---
+++ 01_BASICS/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score=100;
+// console.log(score);
+
+const balance=new Number(500.02568);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber=123.055465;
+
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds= 145646543468476;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//**/***/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/ */ */
+
+// console.log(Math);
+// console.log(Math.abs(-456));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(5.99));
+// console.log(Math.round(4645.6546484));
+// console.log(Math.min(4,5,8,2341658,25,1));
+// console.log(Math.max(4,5,8,2341658,25,1));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
