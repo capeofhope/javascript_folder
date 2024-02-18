8f02b6d6d796fa5c98940388312aa5b888974014
capeofhope <98865989+capeofhope@users.noreply.github.com>
2024-02-18 10:00:52 +0000

    comparison_operators

---
+++ 01_BASICS/04_comparisons.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//console.log("2"+1);(error)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+//===(strictly equal)
+//used for chevcking values as weel as data_types
+console.log("2"==2);//true
+console.log("2"===2);//false
