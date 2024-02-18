69421904cafa24bcb694d0ce06636ce45b646f7d
capeofhope <98865989+capeofhope@users.noreply.github.com>
2024-02-18 09:08:10 +0000

    operation_on_data_types

---
+++ 01_BASICS/03_conversionOperation.js
@@ -0,0 +1,51 @@
+let score=null;
+// console.log(typeof score);
+
+let valueNumber=Number(score);
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+//when null is coverted to number
+//when undefined is converted into number gives NaN
+//"33"=>33
+//"33abc"=>NaN
+//null=>0
+//true=>1
+//false=>0
+//undefined=>NaN
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//***************Operations ****************/
+let value=3;
+// console.log(-value);
+
+let str1="Mrinal";
+let str2=" Kandwal";
+// console.log(str1+str2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+"2"+2);
+
+// console.log(+true);
+// console.log(+"");
+
+//postfix increment
+let x = 3;
+const y = x++;
+console.table([x,y]);
+// x is 4; y is 3
+
+//prefix increment
+let x2 = 3n;
+const y2 = ++x2;
+console.table([x2,y2]);
+// x2 is 4n; y2 is 4n
+
