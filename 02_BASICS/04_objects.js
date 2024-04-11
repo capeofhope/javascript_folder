71fdc1ee6d7379b4661c3377cda86ee04f57f888
capeofhope <98865989+capeofhope@users.noreply.github.com>
2024-04-11 07:26:48 +0000

    object 2nd lecture

---
+++ 02_BASICS/04_objects.js
@@ -0,0 +1,49 @@
+// const tinderUser= new Object();//singelton object
+const tinderUser={};//non-singelton object
+
+tinderUser.id="123abc";
+tinderUser.name="Mrinal";
+tinderUser.isLoggedIn=false;
+
+// console.log(tinderUser);
+
+const regularUser={
+    email:"some@gmail.com",
+    fullname:{
+        userFullName:{
+            firstname:"Mrinal",
+            lastname:"Kandwal",
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName)
+
+const obj1={1:"a",2:"b"};
+const obj2={3:"a",4:"b"};
+
+// const obj3={obj1,obj2};//mt use krio
+// const obj3=Object.assign({},obj1,obj2)// ye krio
+const obj3={...obj1,...obj2}//ye or aasand hai
+// console.log(obj3);
+
+const users=[
+    {
+        id:1,
+        email:"mrinal@gmail.com"
+    },
+    {
+        id:1,
+        email:"mrinal@gmail.com"
+    },
+    {
+        id:1,
+        email:"mrinal@gmail.com"
+    }
+];
+
+// console.log(tinderUser);
+console.log(Object.keys(tinderUser));//keys
+console.log(Object.values(tinderUser));//values
+console.log(Object.entries(tinderUser));//all enteries [[key, values]]
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));//
